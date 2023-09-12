83d813ed1dbfc1c0086b54c189185e826d5aeee2
Milena <bethmarin10@gmail.com>
2023-09-12 13:00:51 -0500

    Css

--- index.php
+++ index.php
@@ -18,6 +18,33 @@ $pdf->writeHTML($utils->format_title($etiquetas->inscripcion_titulo));
 $pdf->SetXY(0,$pdf->GetY()+10); 
 $pdf->writeHTML($utils->format_question($etiquetas->inscripcion_p1));
     
+
+$pdf->SetXY(10,$pdf->GetY()+10); 
+
+$tipo_registro = 1;
+if($tipo_registro ==1){//personal
+	$tb_tipo = '<table cellspacing="0" cellpadding="1" border="1" style="width: 100 %; margin-left: auto; margin-right: auto; margin-top: 50%">';
+	$tb_tipo.= "<tbody>";
+	$tb_tipo.= '<tr>';
+	$tb_tipo.= '<td style="height:25px; color: #FFFFFF; background-color: #000000; display: flex; align-items:center">	<span>'.$etiquetas->inscripcion_p1_op2.'</span></td>';
+	$tb_tipo.= "<td>".$etiquetas->inscripcion_p1_op1."</td>";
+	$tb_tipo.= "</tr>";
+	$tb_tipo.= "</tbody>";
+	$tb_tipo.= "</table>";
+	$pdf->writeHTML($tb_tipo);
+}else{//organización
+	$tb_tipo = "<table cellspacing='0' cellpadding='1' border='1' style='width: 100 %; margin-left: auto; margin-right: auto;'>";
+	$tb_tipo.= "<tbody>";
+	$tb_tipo.= "<tr>";
+	$tb_tipo.= "<td>".$etiquetas->inscripcion_p1_op2."</td>";
+	$tb_tipo.= '<td style="color:  #000000; background-color: #BDB7B7;">'.$etiquetas->inscripcion_p1_op1.'</td>';
+	$tb_tipo.= "</tr>";
+	$tb_tipo.= "</tbody>";
+	$tb_tipo.= "</table>";
+	$pdf->writeHTML($tb_tipo);
+
+}
+
 $pdf->SetXY(0,$pdf->GetY()+10);
 $pdf->writeHTML($utils->format_question($etiquetas->inscripcion_p2_sub2));
 
@@ -145,10 +172,85 @@ $tb_datos.= "</table>";
 $pdf->writeHTML($tb_datos);
 
 
+$pdf->AddPage();
+
 $pdf->SetXY(0,$pdf->GetY()+10);
 $pdf->writeHTML($utils->format_question($etiquetas->inscripcion_p5));
 
+$pdf->SetXY(10,$pdf->GetY()+10);
+
+$tb_datos = "<table cellspacing='0' cellpadding='1' border='1' style='width: 100 %; margin-left: auto; margin-right: auto;'>";
+$tb_datos.= "<tbody>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Redes Sociales de Premios Verdes</td>";
+$tb_datos.= "</tr>";
+
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Redes Sociales de Amigos/Organizaciones</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Medios de Comunicación</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Un Mail directo de Premios Verdes</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Un Mail de Organización aliada</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Publicidad</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Recomendación</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Ya es miembro de la comunidad Premios Verdes</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "</tbody>";
+$tb_datos.= "</table>";
+$pdf->writeHTML($tb_datos);
+
+
 $pdf->SetXY(0,$pdf->GetY()+10);
+$pdf->writeHTML($utils->format_question($etiquetas->inscripcion_p6));
+
+$pdf->SetXY(10,$pdf->GetY()+10);
+
+$tb_datos = "<table cellspacing='0' cellpadding='1' border='1' style='width: 100 %; margin-left: auto; margin-right: auto;'>";
+$tb_datos.= "<tbody>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Privado</td>";
+$tb_datos.= "</tr>";
+
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Publico</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Tercer Sector</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "<tr>";
+$tb_datos.= "<td>Alianza Público-Privada</td>";
+$tb_datos.= "</tr>";
+
+$tb_datos.= "</tbody>";
+$tb_datos.= "</table>";
+$pdf->writeHTML($tb_datos);
+
+
+
 
 //$pdf->writeHTML("<table>");
 //$pdf->writeHTML("<tr>");
@@ -162,7 +264,6 @@ $pdf->SetXY(0,$pdf->GetY()+10);
 //$pdf->SetX($pdf->GetX()+10);
 //$pdf->writeHTML($utils->format_question("Milena Marin"));
 
-$pdf->writeHTML($utils->format_question("Milena Marin"));
 
 ob_end_clean();
 $pdf->Output('proyecto.pdf', 'I');
